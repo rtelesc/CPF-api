e327413b20f404a3f503c2b42b7ba196fb99da44
Adriano Laureano <adriano741@rocketmail.com>
2015-05-31 11:27:09 -0300

    Update index.php

--- example-receita/index.php
+++ example-receita/index.php
@@ -1,17 +1,32 @@
 <?php
-
-require_once("../classes/cpf-Receita.php");
+require_once ("../classes/cpf-Receita.php");
 
 $cpf = new ConsultaCPF();
 
-$cpf->SetCPF("13326724691");
+if (empty($_GET['cpf']))
+    {
+    $cpf->SetCPF("13326724691"); // CPF exemplo
+    if ($cpf->consultar())
+        {
+        print $cpf->GetXml() . "\n";
+        }
+      else
+        {
+        print "Consulta falhou: \n";
+        print $cpf->error() . "\n";
+        }
+    }
 
-if($cpf->consultar()){
-
-print $cpf->GetXml()."\n";
-} else {
-
-print "Consulta falhou: \n";
-
-print $cpf->error()."\n";
-}
+if (!empty($_GET['cpf']))
+    {
+    $cpf->SetCPF($_GET['cpf']);
+    if ($cpf->consultar())
+        {
+        print $cpf->GetXml() . "\n";
+        }
+      else
+        {
+        print "Consulta falhou: \n";
+        print $cpf->error() . "\n";
+        }
+    }
